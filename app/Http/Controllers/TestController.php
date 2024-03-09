bf54b6b05f98e84ba0188984812d265835930828
Jordi <jordijs6@gmail.com>
2024-03-09 20:22:51 +0100

    Fix: modify migrations to make foreign keys work

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    // public function show(string $id): View
+    // {
+    //     return view('user.profile', [
+    //         'user' => User::findOrFail($id)
+    //     ]);
+    // }
+}
--- database/migrations/2024_03_09_183914_create_grades_table.php
+++ database/migrations/2024_03_09_183914_create_grades_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('grades', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->foreignIdFor(Student::class);
-            $table->foreignIdFor(Subject::class);
-            $table->float('grade', $precision = 4, $scale = 2);
+            $table->foreignId('student_id')->constrained();
+            $table->foreignId('subject_id')->constrained();
+            $table->float('grade', $precision = 4, $scale = 2)->nullable(true);
         });
     }
 
