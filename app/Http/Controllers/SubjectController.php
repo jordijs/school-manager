fadba5e7d0a06daaeb2f3bff71c61d6baee9a6d8
Jordi <jordijs6@gmail.com>
2024-03-10 00:18:01 +0100

    Feature: Create an API endpoint to create a new subject

---
+++ app/Http/Controllers/SubjectController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subject;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class SubjectController extends Controller
+{
+    public function create(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'school_year' => 'required|in:1,2,3,4'
+            ]);
+
+            $subject = Subject::create([
+                'name' => $request->name,
+                'school_year' => $request->school_year,
+            ]);
+
+            return response([
+                'subject' => $subject,
+                'message' => 'Subject created successfully'
+            ], 200);
+        } catch (ValidationException $exception) {
+            return response([
+                'errors' => $exception->errors(),
+                'message' => 'Validation failed'
+            ], 422);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\StudentController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\SubjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,3 +27,4 @@ use Illuminate\Support\Facades\Route;
 }
 */
 Route::post('/students', [StudentController::class, 'create']);
+Route::post('/subjects', [SubjectController::class, 'create']);
