836aec74d882b5137dd07c02ddbec6cc9cba993d
Jordi <jordijs6@gmail.com>
2024-03-09 19:53:38 +0100

    Complete migrations

--- database/migrations/2024_03_09_183914_create_grades_table.php
+++ database/migrations/2024_03_09_183914_create_grades_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Student;
+use App\Models\Subject;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,11 +13,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('subjects', function (Blueprint $table) {
+        Schema::create('grades', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->enum('school_year', [1, 2, 3, 4]);
             $table->timestamps();
+            $table->foreignIdFor(Student::class);
+            $table->foreignIdFor(Subject::class);
+            $table->float('grade', $precision = 4, $scale = 2);
         });
     }
 
@@ -24,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('subjects');
+        Schema::dropIfExists('grades');
     }
 };
