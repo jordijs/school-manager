783ae5c17ef048b7f67771e75ff974057fd32f36
Jordi <jordijs6@gmail.com>
2024-03-15 01:01:24 +0100

    fix: solve problems with database refresh using sqlite / test: add tests for update and delete student

--- tests/Feature/StudentsTest.php
+++ tests/Feature/StudentsTest.php
@@ -2,12 +2,12 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class StudentsTest extends TestCase
-
 
 {
     use RefreshDatabase;
@@ -19,7 +19,7 @@ class StudentsTest extends TestCase
             'birthday' => '1990-01-01'
         ];
 
-        $response = $this->post('/api/students', $fakeData );
+        $response = $this->post('/api/students', $fakeData);
 
         $response->assertStatus(201);
         $response->assertJsonFragment($fakeData);
@@ -28,7 +28,6 @@ class StudentsTest extends TestCase
     public function test_API_returns_all_students(): void
     {
         $this->seed();
-
         $response = $this->get('/api/students');
         $response->assertStatus(200);
         $response->assertJsonStructure([
@@ -41,6 +40,45 @@ class StudentsTest extends TestCase
                 ]
             ]
         ]);
+    }
+
+    public function test_API_returns_a_student(): void
+    {
+        $this->seed();
+        $response = $this->get('/api/students/1');
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'student' => [
+                'id',
+                'name',
+                'surname',
+                'birthday'
+            ]
+        ]);
+    }
+
+    public function test_API_updates_data_of_student(): void
+    {
+        $this->seed();
+
+        $fakeData = [
+            'name' => 'FakeName',
+            'surname' => 'FakeSurname',
+            'birthday' => '1990-01-01'
+        ];
+
+        $response = $this->put('/api/students/1', $fakeData);
+
+        $response->assertStatus(200);
+        $response->assertJsonFragment($fakeData);
+    }
+
+    public function test_API_deletes_a_student(): void
+    {
+        $this->seed();
+        $response = $this->delete('/api/students/1');
+        $response->assertStatus(200);
+        $response->assertJsonFragment(['message' => 'Student deleted successfully']);
     }
 
     // public function test_API_returns_grades_of_student(): void
